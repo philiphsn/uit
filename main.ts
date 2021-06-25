0367baf8d0a960c8c833f6c21b2c8c15cdf85030
philiphsn <84082347+philiphsn@users.noreply.github.com>
2021-06-25 13:57:25 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,30 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-})
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-    led.plot(0, 0)
-basic.forever(function () {
-    basic.showNumber(0)
-    basic.showIcon(IconNames.Heart)
-})
+enum angleType{
+    radians=1,
+    degrees=2,
+}
+namespace trigonometry {
+
+    // Functions for trigonometry
+
+    /**
+    * Reads the number
+    */
+    //% weight=30 blockId="trigonometry_sine" block="Get sine of pin %x | in %angleType"
+    export function sine(x: number, type: angleType): number {
+    
+        switch(type){
+            case angleType.radians: return calculateSine(x, true)
+            case angleType.degrees: return  calculateSine(x, false)
+            default: return null
+        }
+    }
+
+    /**
+     * Function to calculate sine
+     */
+    //% shim=gatorMoisture::getMoisture
+    function calculateSine(angle: number, dim: boolean) {
+        // Use series
+        return 0
+    }
+}
