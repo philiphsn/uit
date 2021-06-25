637d7a88202fa6454bfc6fbb5e84ceb577f5cd95
philiphsn <84082347+philiphsn@users.noreply.github.com>
2021-06-25 14:15:54 +0800

    a lot

--- main.ts
+++ main.ts
@@ -3,16 +3,15 @@ enum angleType{
     degrees=2,
 }
 namespace trigonometry {
-
     // Functions for trigonometry
 
     /**
-    * Reads the number
+    * Reads the angle number based on dimension, radians or degrees
     */
     //% weight=30 blockId="trigonometry_sine" block="sine of angle %x | in %angleType"
-    export function sine(x: number, type: angleType): number {
+    export function sine(x: number, Dim: angleType): number {
     
-        switch(type){
+        switch(Dim){
             case angleType.radians: return calculateSine(x, true)
             case angleType.degrees: return  calculateSine(x, false)
             default: return null
@@ -23,9 +22,17 @@ namespace trigonometry {
      * Function to calculate sine using Taylor series
      */
     function calculateSine(angle: number, dim: boolean) {
-        // Use Taylor or Maclaurin series
+        // Use Taylor or Maclaurin series upto 6 terms
         return 0
     }
+
+    function degToRad(degrees: number) {
+     return degrees * (Math.PI / 180);
+    };
+
+    function radToDeg(rad: number) {
+        return rad / (Math.PI / 180);
+    };
 
     function factorial(n: number) {
         let answer = 1;
